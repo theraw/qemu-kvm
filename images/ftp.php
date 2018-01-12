4704b91ec15956e5cd03440aaeb339a80c8d7a8d
ƬHE ЯAW ☣ <raw@dopehosting.net>
2018-01-12 21:33:47 +0000

    Update and rename ftp.json to ftp.php

---
+++ images/ftp.php
@@ -0,0 +1,35 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+$json_array = array(
+// ========================================================================================================
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ========================================================================================================
+    'UBUNTU' => array(
+             '12.04' => "Ubuntu 12.04 => ftp://ftp.ubuntu.com/ubuntu/dists/precise/main/installer-amd64/",
+             '14.04' => "ftp://ftp.ubuntu.com/ubuntu/dists/trusty/main/installer-amd64/",
+             '16.04' => "ftp://ftp.ubuntu.com/ubuntu/dists/xenial/main/installer-amd64/",
+             '16.10' => "ftp://ftp.ubuntu.com/ubuntu/dists/yakkety/main/installer-amd64/",
+             '17.04' => "ftp://ftp.ubuntu.com/ubuntu/dists/zesty/main/installer-amd64/",
+             '17.10' => "ftp://ftp.ubuntu.com/ubuntu/dists/artful/main/installer-amd64/",
+             '18.04' => "ftp://ftp.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/"
+    ),
+    'DEBIAN' => array(
+             '7' => "http://ftp.debian.org/debian/dists/wheezy/main/installer-amd64/",
+             '8' => "http://ftp.debian.org/debian/dists/jessie/main/installer-amd64/",
+             '9' => "http://ftp.debian.org/debian/dists/Debian9.3/main/installer-amd64/"
+    ),
+    'DEBIAN' => array(
+             '6' => "http://mirror.centos.org/centos/6/os/x86_64/",
+             '7' => "http://mirror.centos.org/centos/7/os/x86_64/"
+    ),
+    'SUSE' => array(
+             '0' => "http://download.opensuse.org/distribution/openSUSE-stable/repo/oss/"
+    ),
+// ========================================================================================================
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ========================================================================================================
+);
+$images = json_encode($json_array);
+echo $images;
+// debug mode to view arrays => 'print_r($images);' or 'var_dump($images);' .
+?>
